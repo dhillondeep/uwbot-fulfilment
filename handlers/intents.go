17e6354a691808b84b05429a55512a9dba1c87a6
Deep Dhillon <deep@deepdhillon.ca>
2020-02-09 02:05:52 -0500

    few enddpoints for course cat implemented

---
+++ handlers/intents.go
@@ -0,0 +1,14 @@
+package handlers
+
+// Course Intents
+const (
+	CourseAvailabilityTerm = "01_course_availability_term"
+	CourseAvailabilityCurrTerm = "02_course_availability_curr_term"
+	CourseScheduleCurrTerm = "03_course_schedule_curr_term"
+	CoursePrerequisites = "04_course_prerequisites"
+)
+
+// Intent categories
+const (
+	CourseIntent = "course"
+)
