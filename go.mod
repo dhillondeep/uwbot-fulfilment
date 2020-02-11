6ab40fd22a4ea13c3dcc14a515194a6c8bae5218
Deep Dhillon <deep@deepdhillon.ca>
2020-02-10 19:58:57 -0500

    added support for AWS lambda

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/Jeffail/gabs/v2 v2.4.0
+	github.com/aws/aws-lambda-go v1.13.3
 	github.com/dhillondeep/go-uw-api v0.0.0-20200209044931-55bdcb491281
 	github.com/gin-gonic/gin v1.5.0
 	github.com/golang/protobuf v1.3.3
--- main.go
+++ main.go
@@ -1,59 +1,59 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/events"
 	"net/http"
 	"os"
+	"strings"
 	"warrior_bot/handlers"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/dhillondeep/go-uw-api"
 	"github.com/gin-gonic/gin"
-	"github.com/golang/protobuf/jsonpb"
 	"github.com/sirupsen/logrus"
-	"google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
 )
 
 var uwApiClient uwapi.UWAPI
 
-// handles webhook requests from dialogflow
-func handleWebhook(c *gin.Context) {
-	var err error
-
-	req := dialogflow.WebhookRequest{}
-	if err = jsonpb.Unmarshal(c.Request.Body, &req); err != nil {
-		logrus.WithError(err).Error("Unable to unmarshal request to jsonpb")
-		c.Status(http.StatusBadRequest)
-		return
-	}
-
-	resp, err := handlers.HandleRequest(&req, &uwApiClient)
+// handles webhook requests from dialogflow using gin
+func ginHandler(c *gin.Context) {
+	respStr, err := handlers.HandleWebhook(c.Request.Body, &uwApiClient)
 	if err != nil {
-		logrus.WithError(err)
-	}
-
-	marshaller := jsonpb.Marshaler{
-		Indent: " ",
-	}
-	respStr, err := marshaller.MarshalToString(resp)
-	if err != nil {
-		logrus.WithError(err).Error("Unable to marshall request to JSON")
 		c.Status(http.StatusBadRequest)
-		return
 	}
 
 	// send response back
 	c.String(http.StatusOK, respStr)
 }
 
+// handles webhook requests from dialogflow using lambda
+func lambdaHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	respStr, err := handlers.HandleWebhook(strings.NewReader(request.Body), &uwApiClient)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body: respStr,
+			StatusCode: http.StatusBadRequest,
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body: respStr,
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
 func main() {
-	var err error
-
-	r := gin.Default()
-	r.POST("/webhook", handleWebhook)
-
 	// create a client for Uwaterloo API
 	uwApiClient = uwapi.Create(os.Getenv("UW_API_KEY"))
 
-	if err = r.Run(); err != nil {
-		logrus.WithError(err).Fatal("Couldn't start server")
+	if len(os.Getenv("AWS_LAMBDA_USE")) != 0 {
+		lambda.Start(lambdaHandler)
+	} else {
+		r := gin.Default()
+		r.POST("/webhook", ginHandler)
+
+		if err := r.Run(); err != nil {
+			logrus.WithError(err).Fatal("Couldn't start server")
+		}
 	}
 }
