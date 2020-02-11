54f8a761436df10345627ace913891ca5214b656
Deep Dhillon <deep@dhillon.io>
2020-02-10 19:58:57 -0500

    added support for AWS lambda

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,11 +3,14 @@ package handlers
 import (
 	"errors"
 	"github.com/dhillondeep/go-uw-api"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
+	"io"
 	"strings"
 )
 
-func HandleRequest(req *dialogflow.WebhookRequest, uwClient *uwapi.UWAPI) (*dialogflow.WebhookResponse, error) {
+func handleRequest(req *dialogflow.WebhookRequest, uwClient *uwapi.UWAPI) (*dialogflow.WebhookResponse, error) {
 	intentCat := strings.Split(req.QueryResult.Intent.DisplayName, "_")[1]
 
 	// we already have fulfilment text provides so we shouldn't do anything
@@ -23,4 +26,31 @@ func HandleRequest(req *dialogflow.WebhookRequest, uwClient *uwapi.UWAPI) (*dial
 	default:
 		return nil, errors.New("handler does not exist for intent category: " + intentCat)
 	}
+}
+
+func HandleWebhook(request io.Reader, uwApiClient *uwapi.UWAPI) (string, error) {
+	var err error
+
+	req := dialogflow.WebhookRequest{}
+	if err = jsonpb.Unmarshal(request, &req); err != nil {
+		logrus.WithError(err).Error("Unable to unmarshal request to jsonpb")
+		return "nil", err
+	}
+
+	resp, err := handleRequest(&req, uwApiClient)
+	if err != nil {
+		logrus.WithError(err)
+	}
+
+	marshaller := jsonpb.Marshaler{
+		Indent: " ",
+	}
+
+	respStr, err := marshaller.MarshalToString(resp)
+	if err != nil {
+		logrus.WithError(err).Error("Unable to marshall request to JSON")
+		return "", err
+	}
+
+	return respStr, nil
 }
