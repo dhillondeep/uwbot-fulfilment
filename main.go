83e92cce26c25ee7f51b62922ad09ad26b7a70c0
Deep Dhillon <deep@dhillon.io>
2020-02-11 01:41:06 -0500

    README updated and minor env code fixup

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	// create a client for Uwaterloo API
 	uwApiClient = uwapi.Create(os.Getenv("UW_API_KEY"))
 
-	if len(os.Getenv("AWS_LAMBDA_USE")) != 0 {
+	if _, exists := os.LookupEnv("AWS_LAMBDA_USE"); exists {
 		lambda.Start(lambdaHandler)
 	} else {
 		r := gin.Default()
